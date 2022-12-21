ad22eaf1f9526dec48a3f8fda3932bc2f2a7dbfc
Joris Vink <joris@coders.se>
2022-12-21 20:13:37 +0100

    missing else for shell kw

--- syntax.c
+++ syntax.c
@@ -211,7 +211,7 @@ static const char *js_other[] = {
 static const char *sh_kw[] = {
 	"if", "fi", "while", "do", "exit", "return",
 	"shift", "case", "esac", "echo", "print", "set",
-	"then", "for", "while", "in", "done", NULL
+	"then", "for", "while", "in", "done", "else", NULL
 };
 
 static const char *go_kw[] = {
